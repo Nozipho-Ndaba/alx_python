4d3e58a7fe4cba9db7c835f2e51c0f2743deda44
Nozipho-Ndaba <Ntombikayisenhlane001@gmail.com>
2023-10-02 08:07:23 -0700

    Task 1 answered

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+
+
+def get_employee_info(employee_id):
+    # Fetch employee details
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(employee_url)
+    employee_data = response.json()
+    employee_name = employee_data['name']
+
+    # Fetch employee TODO list
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(todo_url)
+    todo_data = response.json()
+
+    # Calculate task progress
+    total_tasks = len(todo_data)
+    completed_tasks = sum(1 for task in todo_data if task['completed'])
+
+    # Display employee TODO list progress
+    print(
+        f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+    for task in todo_data:
+        if task['completed']:
+            print(f"\t{task['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_info(employee_id)
